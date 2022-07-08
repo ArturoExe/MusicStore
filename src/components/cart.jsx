5d9c4b18e1c10fb6f124066097784e925b5d1ab2
SebastianRuiz18 <sebastian.ruiz18@tectijuana.edu.mx>
2022-07-08 16:11:57 -0700

    update

--- src/components/cart.jsx
+++ src/components/cart.jsx
@@ -3,6 +3,10 @@ import Country from "./country";
 import Cartprod from "./cartprod";
 import Paybtn from "./paybtn";
 
+import { MdAlternateEmail } from "react-icons/md";
+import { BsFillCreditCard2BackFill } from "react-icons/bs";
+import { BsPerson } from "react-icons/bs";
+
 export const Cart = () => {
     return (
       <div className="cart-container">
@@ -27,17 +31,18 @@ export const Cart = () => {
                 <p>Complete your purchase by providing your payment <br/> detailers order.</p>
                     <form className="card-info">
                         <label for="email">Email Address</label><br/>
-                        <input className="email-input" placeholder="@" type="text" id="email" name="email"/><br/>
+                        
+                        <span className="at-icon">< MdAlternateEmail/></span><input className="email-input" type="text" id="email" name="email"/><br/>
 
                         <div className="card-detail">
-                            <label for="card-number">Card Detail</label><br/>
-                            <input className="card-number" placeholder="Number" type="text" id="card-number" name="card-number"/>
+                        <label for="card-number">Card Detail</label><br/>
+                            <span className="card-icon">< BsFillCreditCard2BackFill/></span><input className="card-number" placeholder="Card Number" type="text" id="card-number" name="card-number"/>
                             <input className="card-expdate" placeholder=" MM / YY" type="text" id="card-expdate" name="card-expdate"/>
                             <input className="card-cvc" placeholder="       CVC" type="text" id="card-cvc" name="card-cvc"/>
                         </div>
                         
                         <label for="card-holder">Card Holder</label><br/>
-                        <input className="cardholder-input" placeholder="Name" type="text" id="card-holder" name="card-holder"/><br/>
+                            <span className="name-icon">< BsPerson/></span><input className="cardholder-input" placeholder="Name" type="text" id="card-holder" name="card-holder"/><br/>
 
                         <div className="billing-adress-info">
                             <label for="billing-address">Billing Address</label><br/>
@@ -47,10 +52,15 @@ export const Cart = () => {
                             <input className="billing-zipcode" placeholder="Zip Code" type="text" id="billing-address" name="billing-address"/>
                         </div>
                     </form>
-                <div className="totals">
-                    <h4>Subtotal    $</h4>
-                    <h4>Tax (20%)    $</h4>
-                    <h4>Total   $</h4>
+                <div className="totals-text">
+                    <h4>Subtotal</h4>
+                    <h4>Tax ( % 20 )</h4>
+                    <h4>Total</h4>
+                </div>
+                <div className="totals-res">
+                    <h4>$ 1000</h4>
+                    <h4>$ 200</h4>
+                    <h4>$ 1200</h4>
                 </div>
                 <Paybtn></Paybtn>
             </div>
--- src/components/paybtn.jsx
+++ src/components/paybtn.jsx
@@ -2,7 +2,7 @@ import "./paybtn.css";
 
 export const Paybtn = () => {
   return (
-    <button class="button-32" role="button">Pay    $</button>
+    <button class="button-80" role="button">Pay $1200</button>
 
   );
 };
